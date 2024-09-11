247a9955d695426164018cf6c53c6ddaacc3631c
ags3780 <hammerhead12as@icloud.com>
2024-09-11 15:18:45 -0500

    Auto Base

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Red2.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+
+@Autonomous(name="RedLong", group="Robot")
+
+
+public class Red2 extends LinearOpMode {
+
+    private DcMotorEx wheelFL;
+    private DcMotorEx wheelFR;
+    private DcMotorEx wheelBL;
+    private DcMotorEx wheelBR;
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+
+    static final double FORWARD_SPEED = 0.6;
+    static final double TURN_SPEED = 0.5;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //Motors
+        wheelFL = hardwareMap.get(DcMotorEx.class, "wheelFL");
+        wheelFR = hardwareMap.get(DcMotorEx.class, "wheelFR");
+        wheelBL = hardwareMap.get(DcMotorEx.class, "wheelBL");
+        wheelBR = hardwareMap.get(DcMotorEx.class, "wheelBR");
+
+
+        wheelFL.setDirection(DcMotorSimple.Direction.REVERSE);//REVERSE
+        wheelFR.setDirection(DcMotorSimple.Direction.FORWARD);//FORWARD
+        wheelBL.setDirection(DcMotorSimple.Direction.FORWARD);//FORWARD
+        wheelBR.setDirection(DcMotorSimple.Direction.REVERSE);//REVERSE
+
+
+        wheelFL.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        wheelFR.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        wheelBL.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        wheelBR.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+
+        waitForStart();
+// Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Ready to run");    //
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+
+    }
+}
